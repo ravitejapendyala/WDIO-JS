dd6a7b944b660d3920c334ebd152f78fa8e7c939
ravitejapendyala <VenkataRaviTeja.Pendyala@blueyonder.com>
2024-05-25 22:19:02 +0530

    Adding wait time

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -23,13 +23,17 @@ Then(/^I should see a flash message saying (.*)$/, async (message) => {
 
 Given('the app is launched', async () => {
     // Wait for the app to load
-    await browser.pause(10000);
+    await driver.pause(12000);
 });
 
 When('the user clicks the button', async () => {
     // Find the button by its resource ID (change to your button's ID)
-    const button = await $('~button_resource_id');
-    await button.click();
+    // const button = await $('~button_resource_id');
+    // await button.click();
+    // Locate the sign-in button by its class name
+    //const signInButton = await $('android=new UiSelector().className("android.view.ViewGroup")');
+    const signInButton = await $('android=new UiSelector().className("android.widget.TextView").text("Sign In")');
+    await signInButton.click();
 });
 
 Then('the result element should be displayed', async () => {
