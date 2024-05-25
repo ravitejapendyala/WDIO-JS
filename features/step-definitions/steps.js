5c0983ce2568a88c97bcb9655136daf0c3703809
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-05-26 00:13:03 +0530

    Update wait timer 4

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -23,7 +23,7 @@ Then(/^I should see a flash message saying (.*)$/, async (message) => {
 
 Given('the app is launched', async () => {
     // Wait for the app to load
-    await driver.pause(10000);
+    await driver.pause(12000);
 });
 
 When('the user clicks the button', async () => {
