cb4b39cc902531025a9f92a764dea112bfbd93a3
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-05-30 18:07:25 +0530

    Add test to identify element using class Name & assert getText

--- features/login.feature
+++ features/login.feature
@@ -23,5 +23,10 @@ Feature: The Internet Guinea Pig Website
     Given the app is launched
     When I Click on app
     Then Action option should be displayed
+  
+  @FindElementByClass
+  Scenario: User opens ApiDemos app
+    Given the app is launched
+    When I get text from first element by className  
     
 
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -3,6 +3,7 @@ import { expect, $ } from '@wdio/globals'
 
 import LoginPage from '../pageobjects/login.page.js';
 import SecurePage from '../pageobjects/secure.page.js';
+import { ALLOWED_SCHEMA_EXTENSIONS } from 'appium/build/lib/schema/schema.js';
 
 const pages = {
     login: LoginPage
@@ -62,6 +63,15 @@ When('I Click on app', async() => {
     await appOption.click();
     await driver.pause(5000);
 })
+
+When(`I get text from first element by className`, async() => {
+    // [When] Describes the action or event that triggers the scenario.
+
+    const className = await $('android.widget.TextView');
+    console.log(await className.getText());
+    await expect(className).toHaveText("API Demos");
+
+});
 
 
 
